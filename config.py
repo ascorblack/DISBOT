4cce45df61bfc3c01157352922eda1693c60424e
Scorpiss <72309540+Scorpiss@users.noreply.github.com>
2021-01-12 23:14:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,3 @@
-class Language:
-    def __init__(self):
-        self.language = 'en-US'
-
-    def __str__(self):
-        return self.language
-
-    def change(self, lang):
-        assert isinstance(lang, str)
-        self.language = lang
-
-language = Language()
-
 settings = {
     'TOKEN': 'Nzk2NjkyNjEyMjQ5NjgxOTMw.X_bn0A.gh3PdwFkOgk74dQfFnHERHLCx3Y',
     'NAME_BOT': 'DBOT',
