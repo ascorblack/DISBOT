1c5ca2ee1076bb355c7e953c85dc975523e19dce
Scorpiss <72309540+Scorpiss@users.noreply.github.com>
2021-01-16 02:38:31 +0300

    F

--- cogs/admin.py
+++ cogs/admin.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions
 import asyncio
 from funcs import guild_id
+from funcs import test
 
 
 class _Admin(commands.Cog):
