a291430b64e963c39f226bed0b880a2828241da3
Scorpiss <72309540+Scorpiss@users.noreply.github.com>
2021-01-16 02:32:29 +0300

    H

--- funcs.py
+++ funcs.py
@@ -6,5 +6,10 @@ from discord.ext import commands
 from discord.ext.commands import has_permissions
 import os
 
-async def guild_id(self, ctx, msg):
+async def guild_id(ctx):
+    guild = str(ctx.guild.id)
+    return guild
+
+async def test(ctx, msg)
+    msg += " да"
     return msg
