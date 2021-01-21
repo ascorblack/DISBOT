7afd3cf6119ee784099cf9060c54dbe005967bb6
Scorpiss <72309540+Scorpiss@users.noreply.github.com>
2021-01-22 02:19:44 +0300

    Up

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import random
 import discord
 import os
 from glob import glob
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.triggers.cron import CronTrigger
+# from apscheduler.schedulers.asyncio import AsyncIOScheduler
+# from apscheduler.triggers.cron import CronTrigger
 from discord import Embed, File, DMChannel
 from discord.ext import commands
 from discord.ext.commands import Bot as BotBase
