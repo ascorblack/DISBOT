0b74f6265d7a398e8ce9ae333d5647e06230a3d5
Scorpiss <72309540+Scorpiss@users.noreply.github.com>
2021-01-24 15:20:58 +0300

    г

--- cogs/Poltest.py
+++ cogs/Poltest.py
@@ -9,6 +9,7 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import time
+import os
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -29,12 +30,13 @@ class Poltest(commands.Cog):
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("window-size=1920,1440")
         browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
         browser.get("https://9axes.github.io/ru/quiz.html")
 
         i = 0
         e = 0
-        while i < 1:
+        while i < 45:
             num = browser.find_element_by_id("question-number").text
             quest = browser.find_element_by_id("question-text").text
             emb = discord.Embed(description=f'**{num}**\n```{quest}```')
@@ -71,9 +73,10 @@ class Poltest(commands.Cog):
                 await ctx.send("Время на ответ истекло!")
                 exit
             i += 1
-        result = browser.find_element_by_id("banner").screenshot_as_png
-        await ctx.send(file=discord.File(result))
-        browser.quit()
+        browser.find_element_by_id("banner").screenshot('resulttest/result.png')
+        emb = discord.Embed(description='Ваш результат')
+        emb.set_image(url='attachment://resulttest/result.png')
+        await msg.edit(embed=emb)
 
 
 def setup(bot):
