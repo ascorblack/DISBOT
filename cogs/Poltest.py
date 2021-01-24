f938f3e0d12ffff59f90fc5f5b8b41c8b3bf723b
Scorpiss <72309540+Scorpiss@users.noreply.github.com>
2021-01-24 12:17:43 +0300

    a

--- cogs/Poltest.py
+++ cogs/Poltest.py
@@ -27,8 +27,8 @@ class Poltest(commands.Cog):
     async def poltest(self, ctx):
         options = webdriver.ChromeOptions()
         # options.headless = True
-        options.binary_location = 'GOOGLE_CHROME_BIN'
-        browser = webdriver.Chrome(options=options)
+        options.binary_location = GOOGLE_CHROME_BIN
+        browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, options=options)
         browser.get("https://9axes.github.io/ru/quiz.html")
 
         i = 0
