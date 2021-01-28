2cf5d093ae19d4d8f742d7d3f547a233f0a9b9bd
Scorpiss <72309540+Scorpiss@users.noreply.github.com>
2021-01-28 12:40:54 +0300

    test

--- mongo.py
+++ mongo.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import os
 
 
-cluster = MongoClient(os.getenv("MONGODB_URI"))
+cluster = MongoClient(os.getenv("MONGODB_URI", 'mongodb://127.0.0.1:27017/DBOT'))
 pref = cluster.DBOT.PREF
 bal = cluster.DBOT.BALANCE
 owners = cluster.DBOT.OWNERS
