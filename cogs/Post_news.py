4ed03584d9c5bc4be470f9de034b998f7e67cda9
Scorpiss <72309540+Scorpiss@users.noreply.github.com>
2021-02-08 01:08:26 +0300

    upd panorama

--- cogs/Post_news.py
+++ cogs/Post_news.py
@@ -6,7 +6,7 @@ from discord.ext.commands import has_permissions
 import asyncio
 from funcs import *
 from mongo import *
-from parse_site import parse_panorama
+from parse_site import get_last_post
 
 
 class Post_news(commands.Cog):
@@ -18,14 +18,15 @@ class Post_news(commands.Cog):
         self.reg_loop.start()
 
     async def loop_post_news(self):
-        last_new = await parse_panorama()
+        last_new = await get_last_post()
         if last_new:
+            print("TRUE")
             for guild in self.bot.guilds:
                 check = self.newschan.find_one({"GuildID": guild.id})["ChannelID"]
                 if check != 0:
                     channel = self.bot.get_channel(check)
-                    for news in last_new:
-                        await channel.send(news)
+                    file = discord.File(open('resulttest/lastpostpanorama.png', 'rb'))
+                    await channel.send(file=file)
 
     @tasks.loop(seconds=30)
     async def reg_loop(self):
--- parse_site.py
+++ parse_site.py
@@ -1,44 +1,33 @@
-from bs4 import BeautifulSoup
-import requests
-import lxml
-import random
-import asyncio
 from selenium import webdriver
 from mongo import lastnews
 import os
 
 
-async def get_last_news(tag):
-    browser.get(f'https://panorama.pub/{tag}')
+async def get_last_post():
+    url = f'https://vk.com/ia_panorama'
 
-    html = browser.page_source
-    print(html)
-    try:
-        soup = BeautifulSoup(html, 'lxml')
-
-        block_search = soup.find('div', class_='news big-previews two-in-row')
-        last_new = block_search.find('a', href=True, class_=True)
-        url_last_new = f'https://panorama.pub{last_new["href"]}'
-        print(url_last_new)
-        return url_last_new
-    except:
-        return "error"
-
-
-async def parse_panorama():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("window-size=1920,1440")
-    global browser
+    chrome_options.add_argument("window-size=1920,5000")
     browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
-    list_news = []
-    for tag in ['politics', 'society', 'science', 'economics']:
-        lastnew = await get_last_news(tag)
-        if lastnews.find_one({"Category": tag})['LastPost'] != lastnew:
-            lastnews.update_one({"Category": tag}, {"$set": {"LastPost": lastnew}})
-            list_news.append(str(lastnew))
 
-    return list_news
+    browser.get(url)
+
+    i = 0
+    for res in browser.find_elements_by_class_name("wall_text"):
+        i += 1
+        if i == 3:
+            for down in browser.find_elements_by_class_name("replies"):
+                i += 1
+                if i == 6:
+                    checktext = res.text
+                    checklast = lastnews.find_one({"Check": "PanoramaVK"})['LastPost']
+                    if str(checklast) != str(checktext) or checklast is None:
+                        lastnews.update_one({"Check": "PanoramaVK"}, {"$set": {"LastPost": str(checktext)}})
+                        res.screenshot('resulttest/lastpostpanorama.png')
+                        return True
+                    else:
+                        return False
