e826feeb5120258c27c330aee29d3b62b535efa7
Scorpiss <72309540+Scorpiss@users.noreply.github.com>
2021-02-10 20:25:17 +0300

    Update Post_news.py

--- cogs/Post_news.py
+++ cogs/Post_news.py
@@ -18,14 +18,11 @@ class Post_news(commands.Cog):
     async def loop_post_news(self):
         last_new = await get_last_post()
         if last_new:
-            print("TRUE")
             for guild in self.bot.guilds:
                 check = self.newschan.find_one({"GuildID": guild.id})["ChannelID"]
                 if check != 0:
                     channel = self.bot.get_channel(check)
-                    print("CHECK")
                     if len(last_new) == 4:
-                        print("CHECK == 4")
                         emb = discord.Embed(title=f"Новость с сайта: {last_new[0]}",
                                             description=f'{last_new[1]}\n\nПродолжение здесь: {last_new[2]}',
                                             color=discord.Colour.blue(), timestamp=datetime.utcnow())
@@ -33,19 +30,14 @@ class Post_news(commands.Cog):
                         await channel.send(embed=emb)
                     else:
                         if len(last_new[1]) > 1:
-                            print("CHECK несколько изобр")
                             photos = last_new[1]
                             await channel.send(f'**Новость взята из: https://vk.com/ia_panorama**'
                                                f'\n{last_new[0]}')
                             for link in photos:
                                 await channel.send(f'{link}')
                         elif last_new[1] is None:
-                            emb = discord.Embed(title=f"Новость взята из: https://vk.com/ia_panorama",
-                                                description=f'{last_new[0]}', color=discord.Colour.blue(),
-                                                timestamp=datetime.utcnow())
-                            await channel.send(embed=emb)
+                            pass
                         else:
-                            print("CHECK одно изобр")
                             emb = discord.Embed(title=f"Новость взята из: https://vk.com/ia_panorama",
                                                 description=f'{last_new[0]}', color=discord.Colour.blue(),
                                                 timestamp=datetime.utcnow())
