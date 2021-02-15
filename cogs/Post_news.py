a9c38bcf7a6aac6a766b72fd810b7528d899232c
Scorpiss <72309540+Scorpiss@users.noreply.github.com>
2021-02-15 16:42:07 +0300

    meduza

--- cogs/Post_news.py
+++ cogs/Post_news.py
@@ -3,7 +3,7 @@ from discord.ext.commands import has_permissions
 import asyncio
 from funcs import *
 from mongo import *
-from parse_site import get_last_post
+from parse_site import get_last_post_panorama_vk, get_last_post_meduza_io
 from datetime import datetime
 
 
@@ -15,8 +15,8 @@ class Post_news(commands.Cog):
         self.lock = asyncio.Lock()
         self.reg_loop.start()
 
-    async def loop_post_news(self):
-        last_new = await get_last_post()
+    async def loop_post_news_panoramavk(self):
+        last_new = await get_last_post_panorama_vk()
         if last_new:
             for guild in self.bot.guilds:
                 check = self.newschan.find_one({"GuildID": guild.id})["ChannelID"]
@@ -36,7 +36,10 @@ class Post_news(commands.Cog):
                             for link in photos:
                                 await channel.send(f'{link}')
                         elif last_new[1] is None:
-                            pass
+                            emb = discord.Embed(title=f"Новость взята из: https://vk.com/ia_panorama",
+                                                description=f'{last_new[0]}', color=discord.Colour.blue(),
+                                                timestamp=datetime.utcnow())
+                            await channel.send(embed=emb)
                         else:
                             emb = discord.Embed(title=f"Новость взята из: https://vk.com/ia_panorama",
                                                 description=f'{last_new[0]}', color=discord.Colour.blue(),
@@ -45,10 +48,20 @@ class Post_news(commands.Cog):
                             emb.set_image(url=link_photo[0])
                             await channel.send(embed=emb)
 
-    @tasks.loop(seconds=800)
+    async def loop_post_news_meduza(self):
+        find = await get_last_post_meduza_io()
+        if find:
+            for guild in self.bot.guilds:
+                check = self.newschan.find_one({"GuildID": guild.id})["ChannelID"]
+                if check != 0:
+                    channel = self.bot.get_channel(check)
+                    await channel.send(find)
+
+    @tasks.loop(seconds=30)
     async def reg_loop(self):
         async with self.lock:
-            await self.loop_post_news()
+            await self.loop_post_news_panoramavk()
+            await self.loop_post_news_meduza()
 
 
 def setup(bot):
--- parse_site.py
+++ parse_site.py
@@ -1,8 +1,11 @@
 import requests
 import json
 from mongo import lastnews
+from bs4 import BeautifulSoup
+import lxml
 
-async def get_last_post():
+
+async def get_last_post_panorama_vk():
     access_token = 'fcfd642dfcfd642dfcfd642d22fc8b3d7fffcfdfcfd642d9cdf1a019291045e6b320489'
     v = '5.126'
     domain = 'ia_panorama'
@@ -10,10 +13,10 @@ async def get_last_post():
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
         'X-Requested-With': 'XMLHttpRequest'
-        }
+    }
 
-
-    get_parse = requests.get(f"https://api.vk.com/method/wall.get?access_token={access_token}&v={v}&domain={domain}", headers=headers)
+    get_parse = requests.get(f"https://api.vk.com/method/wall.get?access_token={access_token}&v={v}&domain={domain}",
+                             headers=headers)
     data = json.loads(get_parse.content)
 
     checklast = lastnews.find_one({"Check": "PanoramaVK"})['LastPost']
@@ -34,7 +37,8 @@ async def get_last_post():
             i = 0
             photo_link = []
             for qu in data["response"]["items"][1]["attachments"]:
-                photo_link.append(f'{data["response"]["items"][1]["attachments"][i]["photo"]["sizes"][max_size]["url"]}')
+                photo_link.append(
+                    f'{data["response"]["items"][1]["attachments"][i]["photo"]["sizes"][max_size]["url"]}')
                 i += 1
         except KeyError:
             photo_link = None
@@ -43,3 +47,22 @@ async def get_last_post():
             return text, photo_link
         else:
             return False
+
+
+async def get_last_post_meduza_io():
+    url = "https://meduza.io/rss2/all"
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+        'X-Requested-With': 'XMLHttpRequest'
+    }
+    api = requests.get(url=url, headers=headers)
+    soup = BeautifulSoup(api.text, "lxml")
+
+    last_news_address = f'{soup.find("item").find("guid").text}'
+    checklast = lastnews.find_one({"Check": "Meduza.io"})['LastPost']
+    if str(checklast) != str(last_news_address):
+        lastnews.update_one({"Check": "Meduza.io"}, {"$set": {"LastPost": str(last_news_address)}})
+
+        return last_news_address
+    else:
+        return False
